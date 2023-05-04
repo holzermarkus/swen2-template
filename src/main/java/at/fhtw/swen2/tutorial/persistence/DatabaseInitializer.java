83cbed4196d378406b3441085119e9ebb9d656b4
Markus Holzer <markus.holzer@hytech.at>
2023-05-04 13:25:32 +0200

    pdf generation with thymeleaf

--- src/main/java/at/fhtw/swen2/tutorial/persistence/DatabaseInitializer.java
+++ src/main/java/at/fhtw/swen2/tutorial/persistence/DatabaseInitializer.java
@@ -2,6 +2,7 @@ package at.fhtw.swen2.tutorial.persistence;
 
 import at.fhtw.swen2.tutorial.persistence.entities.PersonEntity;
 import at.fhtw.swen2.tutorial.persistence.repositories.PersonRepository;
+import at.fhtw.swen2.tutorial.service.dto.Person;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,10 +18,21 @@ public class DatabaseInitializer implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        personRepository.saveAll(getInitialDemoData());
+    }
+
+    public static List<PersonEntity> getInitialDemoData() {
         List<PersonEntity> personList = new ArrayList<>();
         personList.add(PersonEntity.builder().id(5L).name("John").isEmployed(true).build());
         personList.add(PersonEntity.builder().id(7L).name("Albert").isEmployed(true).build());
         personList.add(PersonEntity.builder().id(11L).name("Monica").isEmployed(true).build());
-        personRepository.saveAll(personList);
+        return personList;
+    }
+    public static List<Person> getInitialDemoDataDtos() {
+        List<Person> personList = new ArrayList<>();
+        personList.add(Person.builder().id(5L).name("John").isEmployed(true).build());
+        personList.add(Person.builder().id(7L).name("Albert").isEmployed(true).build());
+        personList.add(Person.builder().id(11L).name("Monica").isEmployed(true).build());
+        return personList;
     }
 }
